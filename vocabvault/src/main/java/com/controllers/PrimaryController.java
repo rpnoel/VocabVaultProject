d859d2c91d27a4d9ae1fe717f1f8c832b46add9f
rpnoel <rpnoel@email.sc.edu>
2024-11-18 06:08:14 -0500

    "Update: XML changes"

--- vocabvault/src/main/java/com/controllers/PrimaryController.java
+++ vocabvault/src/main/java/com/controllers/PrimaryController.java
@@ -3,11 +3,13 @@ package com.controllers;
 import java.io.IOException;
 import javafx.fxml.FXML;
 import com.vocabvault.App;
+import com.narraration.*;
 
 public class PrimaryController {
 
     @FXML
     private void switchToSecondary() throws IOException {
+        Narriator.playSound("¡Hola Mundo!");
         App.setRoot("secondary");
     }
 }
